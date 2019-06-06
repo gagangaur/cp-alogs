e5a03192275eaef58841cc872c0e17a55b464868
gagangaur <gagan1999gaur@gmail.com>
2019-06-06 23:35:58 +0530

    cycle detection using BFS

---
+++ graph/CycleDetection.java
@@ -0,0 +1,77 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            int ver = sc.nextInt();
+            int edg = sc.nextInt();
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            for(int i=0;i<=ver;i++){
+                list.add(i,new ArrayList<Integer>());
+            }
+            for(int i=0;i<edg;i++){
+              int v1 = sc.nextInt();
+              int v2 = sc.nextInt();
+              list.get(v1).add(v2);
+              list.get(v2).add(v1);
+            }
+            System.out.println(list+" "+list.size());
+            boolean result = new DetectCycle().BFS_isCyclic(list, ver);
+            if(result==false){
+              System.out.println("Loop Exsists");
+            }
+            else{
+              System.out.println("Loop donot exsists");
+            }
+        }
+        sc.close();
+    }
+}
+class DetectCycle{
+  public boolean BFS_isCyclic(ArrayList<ArrayList<Integer>> list,int ver){
+    int[] vis = new int[ver+1];
+    new Helper().initialization(vis);
+    // new Helper().print(vis);
+    Queue<Integer> q = new LinkedList<Integer>();
+    ArrayList<Integer> tmp = new ArrayList<Integer>();
+    tmp = list.get(0);
+    for(int i : tmp){
+      q.add(i);
+      vis[i]=0;
+    }
+    // q.add(0);
+    // vis[0]=0;
+    while(q.size()!=0){
+      int pop = q.poll();
+      vis[pop]=1;
+      ArrayList<Integer> arr = new ArrayList<Integer>();
+      arr = list.get(pop);
+      for(int i: arr){
+        if(vis[i]==0){
+          return false;
+        }
+        else{
+          if (vis[i]==1);
+        else{
+          q.add(i);
+          vis[i]=0;}
+        }
+      }
+    }
+    return true;
+  }
+}
+class Helper{
+  public void initialization(int[] arr){
+    for(int i=0;i<arr.length;i++){
+      arr[i]=-1;
+    }
+  }
+  public void print(int[] arr){
+    for(int i:arr){
+      System.out.print(i+" ");
+    }
+    System.out.println();
+  }
+}
